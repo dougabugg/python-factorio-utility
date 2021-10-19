8ae04cdaa0d821a4b6deea56291fb8b90cf01a63
Doug <drezabek@protonmail.com>
2021-10-19 17:56:43 -0500

    added more recipes and tests

--- test_vanilla.py
+++ test_vanilla.py
@@ -11,5 +11,9 @@ assert_raw_inputs("red science", 1, {"iron": 2, "copper": 1})
 assert_raw_inputs("green science", 1, {"iron": 5.5, "copper": 1.5})
 assert_raw_inputs("grey science", 2, {"iron": 9, "copper": 5, "steel": 1, "coal": 10, "brick": 10})
 assert_raw_inputs("blue science", 2, {"iron": 6, "copper": 15, "engine": 2, "plastic": 6, "sulfur": 1})
+assert_raw_inputs("purple science", 3, {
+    "iron": 32.5, "copper": 57.5, "steel": 25, "plastic": 20, "stone": 15, "brick": 10})
+assert_raw_inputs("yellow science", 3, {
+    "iron": 3, "copper": 64.5, "steel": 7, "plastic": 15, "battery": 2, "blue circuit": 2, "electric engine": 1})
 
 print("done. finished without errors")
